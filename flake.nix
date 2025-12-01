977e377fe1989ee9fd2233c08bf0ae2cb13c152e
MidnightCI <midnight-automation+midnightci@iohk.io>
2025-12-01 00:08:03 +0000

    chore(deps): update nixpkgs to nixos-25.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Midnight DApp Connector API";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     utils.url = "github:numtide/flake-utils";
   };
 
